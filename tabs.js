477c409c6c5e6773ea0131aba3d81cc3b2eef7d6
Luke Hansford <l.s.hansford@gmail.com>
2014-04-30 22:05:24 +1000

    Added functionality to tabs

--- tabs.js
+++ tabs.js
@@ -1,11 +1,15 @@
 function showNormal(){
 	document.getElementById("markdown").style.display = 'block';
 	document.getElementById("github").style.display = 'none';
+	document.getElementById("tab1").parentNode.className = 'active-tab'
+	document.getElementById("tab2").parentNode.className = ''
 }
 
 function showGithub(){
 	document.getElementById("github").style.display = 'block';
 	document.getElementById("markdown").style.display = 'none';
+	document.getElementById("tab1").parentNode.className = ''
+	document.getElementById("tab2").parentNode.className = 'active-tab'
 }
 window.onload = function(){
 	document.getElementById("markdown").style.display = 'none';
