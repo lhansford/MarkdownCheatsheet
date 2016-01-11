a2ba66793be6934c334b771dc709f14759cc576c
Luke Hansford <luke@klinify.com>
2016-01-11 09:53:38 +0800

    Cleaned up code a bit and optimised icon.

--- tabs.js
+++ tabs.js
@@ -1,19 +1,22 @@
 function showNormal(){
+	console.log('norm');
 	document.getElementById("markdown").style.display = 'block';
 	document.getElementById("github").style.display = 'none';
-	document.getElementById("tab1").parentNode.className = 'active-tab'
-	document.getElementById("tab2").parentNode.className = ''
+	document.getElementById("tab1").className = 'active-tab'
+	document.getElementById("tab2").className = ''
 }
 
 function showGithub(){
+	console.log('git');
 	document.getElementById("github").style.display = 'block';
 	document.getElementById("markdown").style.display = 'none';
-	document.getElementById("tab1").parentNode.className = ''
-	document.getElementById("tab2").parentNode.className = 'active-tab'
+	document.getElementById("tab1").className = ''
+	document.getElementById("tab2").className = 'active-tab'
 }
+
 window.onload = function(){
 	document.getElementById("markdown").style.display = 'none';
-	document.getElementById("tab1").addEventListener("click",showNormal);
-	document.getElementById("tab2").addEventListener("click",showGithub);
-	showNormal();
+	document.getElementById("tab1").addEventListener("click", showNormal);
+	document.getElementById("tab2").addEventListener("click", showGithub);
+	// showNormal();
 }
